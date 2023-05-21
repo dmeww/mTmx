ea46c1cb7e91658133b21e3dd92b011ae4281f04
dygticky <dygticky@gmail.com>
2023-05-21 15:12:47 +0800

    add get ipv6 by path

--- data_collect/app.py
+++ data_collect/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # 存储数据的列表
 data_list = {}
 
+
 @app.route('/')
 def index():
     return render_template('index.html',datas=data_list)
@@ -18,6 +19,15 @@ def data():
     data['update'] = time.strftime("%m-%d %H:%M:%S", time.localtime())
     data_list[data['device']] = data
     return 'Data received and stored successfully!'
+
+
+@app.route('/ip/<device>', methods=['GET','POST'])
+def get_ip(device):
+    try:
+        return data_list['Reno 6']['ipv6'][:19]+data_list[device]['ipv6']
+    except:
+        return 'err'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5005,debug=True)
