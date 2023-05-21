0c8621860fd7eeef30f412ec9c67dc8cb26b71ab
dygticky <dygticky@gmail.com>
2023-05-21 14:37:25 +0800

    update ipv6 get method

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ def get_info():
     # 获取设备 IP V4 信息
     ip_4_info = os.popen("ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1").read().strip()
 
-    ip_6_info = os.popen("ip -6 address show wlan0 | awk '/inet6/ {print $2}' | grep -v 'fe80'").read().strip()
+    ip_6_info = os.popen("ip -6 addr show dev wlan0 | grep -E 'inet6 .* global' | awk '{print substr($2,20)}' | sort -u").read().strip()
     # 获取用户信息
     user_info = os.popen("whoami").read().strip()
     data['ipv4'] = ip_4_info
@@ -24,16 +24,13 @@ def get_info():
 import requests,json
 
 calc = 0
-
+get_info()
 while True:
-    if calc < 20:
-        # 刚启动一会，多次刷新
-        time.sleep(5)
-        calc+=1
+    if calc != 0:
+        time.sleep(30*60)
     else:
-        # 稳定运行后，1小时1更新
-        time.sleep(3600)
-        calc=1
+        calc +=1
+
     get_info()
 
     try:
